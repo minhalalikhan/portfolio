a6b0320ba0090a7b8b3d22f7af8b39b476915426
minhalalikhan <minhalalikhan.786@gmail.com>
2025-01-02 14:58:38 +0530

    base path set to github url

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import path from 'path'
 // https://vite.dev/config/
 export default defineConfig({
     plugins: [react()],
-    base: './',
+    base: 'https://minhalalikhan.github.io/portfolio/',
     build: {
         outDir: 'docs', // This is the default, you can adjust it if needed
     },
