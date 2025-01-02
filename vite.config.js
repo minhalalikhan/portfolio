4e8a8cc2c9033227c52e9276f8868be355b7df9e
minhalalikhan <minhalalikhan.786@gmail.com>
2025-01-02 14:34:38 +0530

    base path updated

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,10 @@ import path from 'path'
 // https://vite.dev/config/
 export default defineConfig({
     plugins: [react()],
+    base: '/portfolio/',
+    build: {
+        outDir: 'docs', // This is the default, you can adjust it if needed
+    },
     resolve: {
         alias: {
             '@': path.resolve(__dirname, './src'), // Map '@' to the 'src' folder
