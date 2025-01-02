2f288ea8815b3169038e60301264908679e80724
minhalalikhan <minhalalikhan.786@gmail.com>
2025-01-02 14:53:32 +0530

    base path set to ./

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.scss'
-import App from './App.jsx'
+import App from './App'
 import { BrowserRouter } from 'react-router'
 
 createRoot(document.getElementById('root')).render(
--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import path from 'path'
 // https://vite.dev/config/
 export default defineConfig({
     plugins: [react()],
-    base: '/portfolio/',
+    base: './',
     build: {
         outDir: 'docs', // This is the default, you can adjust it if needed
     },
